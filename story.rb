f94d38e9c34706b851d157d46c09fcefdcafdab8
jnappy <jpnappy@gmail.com>
2015-06-27 17:05:48 -0400

    Ive made my first commit

---
+++ story.rb
@@ -0,0 +1,16 @@
+require 'pry'
+
+def display_story(title, likes)
+  puts("Story: #{title}, Likes: #{likes}")
+end
+ 
+#show the user a message asking them for a title
+ 
+#store the input in a variable 
+ 
+#show the user a message asking for how many likes they think it should start with
+ 
+# store the input in a variable 
+ 
+display_story(user_title, user_likes)
+
