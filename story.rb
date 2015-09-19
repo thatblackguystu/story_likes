43e7842fd641b7e0d4f1fdb39acbf82466efccde
Jay <jpnappy@gmail.com>
2015-09-19 16:02:31 -0400

    changed it up

--- story.rb
+++ story.rb
@@ -3,9 +3,6 @@ require 'pry'
 #write a method that accepts two parameters: title and rating
   #the method should print the story title and the rating out of ten
 
-def display_story(title, rating_score)
-  puts("Story: #{title}, : Rating#{rating_score}")
-end
 
 #show the user a message asking them for a title
 
