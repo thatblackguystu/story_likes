6f2513f3ac17079960fca1c5956e6d77b9ddb3b6
Jay <jpnappy@gmail.com>
2015-09-19 16:01:16 -0400

    made some adjustments to this exercise

--- story.rb
+++ story.rb
@@ -1,16 +1,18 @@
 require 'pry'
 
-def display_story(title, likes)
-  puts("Story: #{title}, Likes: #{likes}")
-end
- 
-#show the user a message asking them for a title
- 
-#store the input in a variable 
- 
-#show the user a message asking for how many likes they think it should start with
- 
-# store the input in a variable 
- 
-display_story(user_title, user_likes)
+#write a method that accepts two parameters: title and rating
+  #the method should print the story title and the rating out of ten
 
+def display_story(title, rating_score)
+  puts("Story: #{title}, : Rating#{rating_score}")
+end
+
+#show the user a message asking them for a title
+
+#store the input in a variable
+
+#show the user a message asking for their rating of the movie out of 100
+
+# store the input in a variable
+
+display_story(user_title, user_rating)
